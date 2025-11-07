8bb6917f1558b9feb358b79b3f9d5d3c5db37019
LucasMoya18 <lucasmoya.david@gmail.com>
2025-11-07 11:06:33 -0300

    cors

--- gestioncitas/settings.py
+++ gestioncitas/settings.py
@@ -33,11 +33,14 @@ ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",        
-    "http://192.168.1.101:3000",
-    "http://127.0.0.1:3000",
-    "https://gestioncitas.vercel.app/"     
+    "*"     
 ]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",        
+#     "http://192.168.1.101:3000",
+#     "http://127.0.0.1:3000",
+#     "https://gestioncitas.vercel.app/"     
+# ]
 
 # AUTH_USER_MODEL = 'api.Usuario'  # Solo si usas usuario personalizado como modelo de autenticación
 
