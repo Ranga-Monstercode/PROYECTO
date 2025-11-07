087938d2e96b3609fc778836419c485b3c603291
LucasMoya18 <lucasmoya.david@gmail.com>
2025-11-07 13:24:09 -0300

    reajusteCors

--- gestioncitas/settings.py
+++ gestioncitas/settings.py
@@ -38,6 +38,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://gestioncitas.vercel.app",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://52.86.212.21"
     # agrega otros orígenes necesarios
 ]
 
@@ -57,19 +58,7 @@ CORS_ALLOW_HEADERS = [
 CORS_EXPOSE_HEADERS = ['Content-Type', 'X-CSRFToken']
 CORS_PREFLIGHT_MAX_AGE = 86400
 
-# ❌ Elimina o comenta esta línea - no es compatible con CORS_ALLOW_ALL_ORIGINS
-# CORS_ALLOWED_ORIGINS = [
-#     "*"     
-# ]
 
-# Si en el futuro necesitas restringir orígenes específicos, usa:
-# CORS_ALLOW_ALL_ORIGINS = False
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",        
-#     "http://192.168.1.101:3000",
-#     "http://127.0.0.1:3000",
-#     "https://gestioncitas.vercel.app"  # ✅ Sin barra final
-# ]
 
 AUTH_USER_MODEL = 'api.Usuario'  # Solo si usas usuario personalizado como modelo de autenticación
 
